677fbd0587c43530b1eaaddedcdfb073bb979d2a
macbookpro <macbookpro@macbookpros-MacBook-Pro.local>
2019-10-22 22:36:43 +0100

    second commit

--- GIT test.R
+++ GIT test.R
@@ -1,2 +1,9 @@
 #this is a test for GIT and GITHUB
 x<- 1+1
+echo "# gis_code2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/AzzaElHajjar/gis_code2.git
+git push -u origin master
+y<-2+2
